479f9b26c90b4294d42a47e11cd1e3c66af718c4
timothy1202 <73386165+timothy1202@users.noreply.github.com>
2026-04-28 12:19:57 +0900

    [level 2] Title: 짝지어 제거하기, Time: 6.04 ms, Memory: 6.27 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int solution(string s)
+{
+    stack<char> st;
+    
+    for(int i=0; i<s.size(); i++)
+    {
+        if(!st.empty() && st.top() == s[i])
+            st.pop();
+        else
+            st.push(s[i]);
+    }
+    
+    return st.empty() ? 1 : 0;
+}
