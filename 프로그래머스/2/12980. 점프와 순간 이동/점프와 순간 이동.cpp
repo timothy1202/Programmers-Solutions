6bf81b1f369baea1ce774ec1a9277b2c38f66b91
timothy1202 <73386165+timothy1202@users.noreply.github.com>
2026-04-29 11:17:53 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.57 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    while(n > 0)
+    {
+        if(n % 2 == 1) 
+            answer++;
+        n /= 2;        
+    }
+    
+    return answer;
+}
