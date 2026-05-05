054bb36f926649a3db88518575a2ad629e55e5a6
timothy1202 <73386165+timothy1202@users.noreply.github.com>
2026-05-05 23:08:39 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -7,14 +7,13 @@ int solution(vector<vector<string>> clothes)
 {
     unordered_map<string, int> c;
     
-    // 카테고리별 의상 수 카운트
+
     for (int i = 0; i < clothes.size(); i++) 
         c[clothes[i][1]]++;
     
-    // (각 카테고리 수 + 1) 모두 곱하기
     int answer = 1;
     for (const auto& [key, value] : c)
         answer *= (value + 1);
     
-    return answer - 1;  // 아무것도 안 입는 경우 제거
+    return answer - 1;  
 }
