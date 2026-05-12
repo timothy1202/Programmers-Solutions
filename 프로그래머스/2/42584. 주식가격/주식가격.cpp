dc98157befdf4d249eb85cb8c18a7256bbb41075
timothy1202 <73386165+timothy1202@users.noreply.github.com>
2026-05-12 12:03:51 +0900

    [level 2] Title: 주식가격, Time: 20.44 ms, Memory: 26.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <stack>
+using namespace std;
+
+vector<int> solution(vector<int> prices) 
+{
+    int n = prices.size();
+    vector<int> answer(n, 0);
+    stack<int> s;
+
+    for (int i = 0; i < n; i++) 
+    {
+        while (!s.empty() && prices[s.top()] > prices[i]) 
+        {
+            answer[s.top()] = i - s.top();
+            s.pop();
+        }
+        s.push(i);
+    }
+
+    while (!s.empty()) 
+    {
+        answer[s.top()] = n - s.top() -1;
+        s.pop();
+    }
+
+    return answer;
+}
